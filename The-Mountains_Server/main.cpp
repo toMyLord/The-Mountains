1eb9da2ede897a4a1563ac0254d90df8b1a4123b
MyLord <xul0rd@qq.com>
2020-02-13 16:52:34 +0800

    add cpu core judgment.

--- The-Mountains_Server/main.cpp
+++ The-Mountains_Server/main.cpp
@@ -11,8 +11,20 @@ int main(int argc, char * argv[]) {
             return 1;
         }
 
-        boost::asio::io_context login_io_context_, game_io_context_;
+        // 确定CPU线程数
+        const int CORES_NUMBER = std::thread::hardware_concurrency();
+        int login_thread_number, game_thread_number;
+        // 根据线程数确定每个服务器分配多少线程
+        if (CORES_NUMBER <= 4) {
+            login_thread_number = 2;
+            game_thread_number = 2;
+        } else {
+            login_thread_number = 2;
+            game_thread_number = CORES_NUMBER - 2;
+        }
 
+
+        boost::asio::io_context login_io_context_, game_io_context_;
 
         // 登录服务器
         AsyncServer<LoginSession> login_server(login_io_context_, std::atoi(argv[1]), false);
@@ -21,8 +33,8 @@ int main(int argc, char * argv[]) {
         log_buffer = '[' + TimeServices::getTime() + "  Server Online]:\tLogin Server is online!";
         LogServices::getInstance()->RecordingBoth(log_buffer, true);
 
-        // 新开7个线程，加上原线程总共8个线程用来处理回调函数
-        for(int i = 0; i < 7; i++) {
+        // 新开login_thread_number - 1个线程，加上原线程总共login_thread_number个线程用来处理登录业务
+        for(int i = 0; i < login_thread_number - 1; i++) {
             std::thread th([&login_io_context_](){ login_io_context_.run(); });
             th.detach();
         }
@@ -34,11 +46,18 @@ int main(int argc, char * argv[]) {
         log_buffer = '[' + TimeServices::getTime() + "  Server Online]:\tGame Server is online!";
         LogServices::getInstance()->RecordingBoth(log_buffer, true);
 
-        // 新开8个线程用来处理回调函数
-        for(int i = 0; i < 8; i++) {
+        // 新开game_thread_number个线程用来处理游戏业务
+        for(int i = 0; i < game_thread_number; i++) {
             std::thread th([&game_io_context_](){ game_io_context_.run(); });
             th.detach();
         }
+
+        log_buffer = '[' + TimeServices::getTime() +
+                "  Core Info]:\tThe maximum number of threads supported by the CPU is " + std::to_string(CORES_NUMBER) +
+                ", Login server use " +  std::to_string(login_thread_number) + " thread, and Game server use " +
+                std::to_string(game_thread_number) + " thread.";
+        LogServices::getInstance()->RecordingBoth(log_buffer, true);
+
 
         login_io_context_.run();
         game_io_context_.run();
